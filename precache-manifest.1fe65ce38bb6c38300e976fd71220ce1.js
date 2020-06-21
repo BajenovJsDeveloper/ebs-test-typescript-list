6a1adeaa6058b4c4ae19272ff664bc7a3faeaf94
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-06-22 00:20:40 +0300

    Updates

---
+++ precache-manifest.1fe65ce38bb6c38300e976fd71220ce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47c537a3ca2e2052573e762aae808dc",
+    "url": "/ebs-test-typescript-list/index.html"
+  },
+  {
+    "revision": "1e5eda0c19789599a51b",
+    "url": "/ebs-test-typescript-list/static/js/2.9538c866.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ebs-test-typescript-list/static/js/2.9538c866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555d380f38c98bbd4e54",
+    "url": "/ebs-test-typescript-list/static/js/main.e533dc2b.chunk.js"
+  },
+  {
+    "revision": "1f875d50859540eecf97",
+    "url": "/ebs-test-typescript-list/static/js/runtime-main.455fbf91.js"
+  }
+]);
